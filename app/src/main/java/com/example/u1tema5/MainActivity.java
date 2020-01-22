c01f019b093d61fd994a447a7726dce411bd6f10
ArlynC <alejandra321.any@gmail.com>
2020-01-21 19:47:03 -0500

    parte1

--- app/src/main/java/com/example/u1tema5/MainActivity.java
+++ app/src/main/java/com/example/u1tema5/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.u1tema5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void mimapa(View view) {
+        startActivity(new Intent(this,MiMapa.class));
+    }
+    public void mipolyline(View view) {
+        startActivity(new Intent(this,MiPolyLine.class));
+    }
+
 }
